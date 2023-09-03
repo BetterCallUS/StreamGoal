d19a253a4843479dd7b3bd7dd68de8c6d4f2ca71
Mani Jalilzadeh <maniidevelop@gmail.com>
2023-09-03 18:14:37 +0330

    My Reymit API Token

--- Code/ReymitAPIs.py
+++ Code/ReymitAPIs.py
@@ -1,20 +1,25 @@
+#Mani Khodas kir bokhor dp 
+
+APIReymit = "ZjcxZTkxZGFiMjRlMTBjN0EvSEdpVXczQTZaYWFUN3ZDNE9oV3Z5K0YrZnpGTERzVEtscHJkS3g5Qmc9s"
+
 # دریافت اطلاعات کاربر صاحب توکن:
-UserData = f"https://api.reymit.ir/user/{API_Token}"
+UserData = f"https://api.reymit.ir/user/{APIReymit}"
 
 # دریافت اطلاعات کامل مربوط به هدف فعلی:
-CurrentGoal = f"https://api.reymit.ir/user/{API_Token}/goal"
+CurrentGoal = f"https://api.reymit.ir/user/{APIReymit}/goal"
 
 # لیست 50 دونیت آخر:
-Last50Donations = f"https://api.reymit.ir/user/{API_Token}/donates/last-donates"
+Last50Donations = f"https://api.reymit.ir/user/{APIReymit}/donates/last-donates"
 
 # لیست بزرگ ترین دونیت ها تفکیک شده بر اساس بازه های زمانی مختلف:
-GreatestDonationsByTime = f"https://api.reymit.ir/user/{API_Token}/donates/top-donates"
+GreatestDonationsByTime = f"https://api.reymit.ir/user/{APIReymit}/donates/top-donates"
 
 # لیست بزرگ ترین دونیت کنندگان ها تفکیک شده بر اساس بازه های زمانی مختلف:
-GreatestDonatorsByTime = f"https://api.reymit.ir/user/{API_Token}/donates/top-donators"
+GreatestDonatorsByTime = f"https://api.reymit.ir/user/{APIReymit}/donates/top-donators"
 
 # لیست حمایت کنندگان ثابت شما در ریمیت تفکیک شده بر اساس بازه های زمانی مختلف:
-LoyalDonators = f"https://api.reymit.ir/user/{API_Token}/donates/regular-donators"
+LoyalDonators = f"https://api.reymit.ir/user/{APIReymit}/donates/regular-donators"
 
 # جمع حمایت های دریافت شده در بازه های زمانی مختلف:
-TotalDonations = f"https://api.reymit.ir/user/{API_Token}/donates/totals"
+TotalDonations = f"https://api.reymit.ir/user/{APIReymit}/donates/totals"
+
